101bd8dbe9bd497dbc9431dbb09c99838b1b0467
IcaroWanderley <iwss@cin.ufpe.br>
2022-03-29 20:45:51 -0300

    oca

---
+++ ee1_template/src/Player.java
@@ -0,0 +1,242 @@
+import com.mpatric.mp3agic.InvalidDataException;
+import com.mpatric.mp3agic.UnsupportedTagException;
+import javazoom.jl.decoder.*;
+import javazoom.jl.player.AudioDevice;
+import javazoom.jl.player.FactoryRegistry;
+import support.PlayerWindow;
+import support.Song;
+
+import java.awt.event.*;
+import java.util.ArrayList;
+
+public class Player {
+
+    /**
+     * The MPEG audio bitstream.
+     */
+    private Bitstream bitstream;
+    /**
+     * The MPEG audio decoder.
+     */
+    private Decoder decoder;
+    /**
+     * The AudioDevice the audio samples are written to.
+     */
+    private AudioDevice device;
+
+    private PlayerWindow window;
+
+    private ArrayList<Song> musica =  new ArrayList <Song>();
+    private boolean repeat = false;
+    private boolean shuffle = false;
+    private boolean playerEnabled = false;
+    private boolean playerPaused = true;
+    private Song currentSong;
+    private int currentFrame = 0;
+    private int newFrame;
+    String[][] fila = {};
+    public Player() {
+        ActionListener buttonListenerPlayNow = new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        };
+        ActionListener buttonListenerRemove = new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        };
+        ActionListener buttonListenerAddSong = new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Thread t_AddSong = new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+                            System.out.println("opa");
+                            Song n_music = window.getNewSong();
+
+                        } catch (Exception ex) {
+
+                        }
+
+                    }
+                });
+                t_AddSong.start();
+            }
+        };
+        ActionListener buttonListenerShuffle = new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        };
+        ActionListener buttonListenerPrevious = new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        };
+        ActionListener buttonListenerPlayPause = new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        };
+        ActionListener buttonListenerStop = new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        };
+        ActionListener buttonListenerNext = new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        };
+        ActionListener buttonListenerRepeat = new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        };
+        MouseListener scrubberListenerClick = new MouseListener() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mousePressed(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+
+            }
+        };
+        MouseMotionListener scrubberListenerMotion =  new MouseMotionListener() {
+            @Override
+            public void mouseDragged(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseMoved(MouseEvent e) {
+
+            }
+        };
+        window = new PlayerWindow("song simulator", fila, buttonListenerPlayNow,
+                buttonListenerRemove,
+                buttonListenerAddSong,
+                buttonListenerShuffle,
+                buttonListenerPrevious,
+                buttonListenerPlayPause,
+                buttonListenerStop,
+                buttonListenerNext,
+                buttonListenerRepeat,
+                scrubberListenerClick,
+                scrubberListenerMotion);
+
+    }
+
+    //<editor-fold desc="Essential">
+
+    /**
+     * @return False if there are no more frames to play.
+     */
+    private boolean playNextFrame() throws JavaLayerException {
+        // TODO Is this thread safe?
+        if (device != null) {
+            Header h = bitstream.readFrame();
+            if (h == null) return false;
+
+            SampleBuffer output = (SampleBuffer) decoder.decodeFrame(h, bitstream);
+            device.write(output.getBuffer(), 0, output.getBufferLength());
+            bitstream.closeFrame();
+        }
+        return true;
+    }
+
+    /**
+     * @return False if there are no more frames to skip.
+     */
+    private boolean skipNextFrame() throws BitstreamException {
+        // TODO Is this thread safe?
+        Header h = bitstream.readFrame();
+        if (h == null) return false;
+        bitstream.closeFrame();
+        currentFrame++;
+        return true;
+    }
+
+    /**
+     * Skips bitstream to the target frame if the new frame is higher than the current one.
+     *
+     * @param newFrame Frame to skip to.
+     * @throws BitstreamException
+     */
+    private void skipToFrame(int newFrame) throws BitstreamException {
+        // TODO Is this thread safe?
+        if (newFrame > currentFrame) {
+            int framesToSkip = newFrame - currentFrame;
+            boolean condition = true;
+            while (framesToSkip-- > 0 && condition) condition = skipNextFrame();
+        }
+    }
+    //</editor-fold>
+
+    //<editor-fold desc="Queue Utilities">
+    public void addToQueue(Song song) {
+        musica.add(song);
+        fila = getQueueAsArray();
+    }
+
+    public void removeFromQueue(String filePath) {
+    }
+
+    public String[][] getQueueAsArray() {
+        String[][] novafila = new String[musica.size()][]
+        return fila;
+    }
+
+    //</editor-fold>
+
+    //<editor-fold desc="Controls">
+    public void start(String filePath) {
+    }
+
+    public void stop() {
+    }
+
+    public void pause() {
+    }
+
+    public void resume() {
+    }
+
+    public void next() {
+    }
+
+    public void previous() {
+    }
+    //</editor-fold>
+
+    //<editor-fold desc="Getters and Setters">
+
+    //</editor-fold>
+}
